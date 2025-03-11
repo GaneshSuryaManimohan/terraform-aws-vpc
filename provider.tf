4257cbcb74bba6a1442aaa8f32fae6c3d3363dbf
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2025-03-11 17:14:16 +0530

    VPC Module

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.86.0"
+    }
+  }
+  backend "s3" {
+    bucket = "daws25s-vpc-s3"
+    key    = "expense-vpc"
+    region = "us-east-1"
+    dynamodb_table = "expense-vpc"
+  }
+}
+#provide authentication here:
+provider "aws" {
+  region = "us-east-1"
+}
